0e0024d4906b1bb6e2d40be9c174f464f0ec28e1
Kyle Guerrero <kyleguerrero85@gmail.com>
2019-12-09 11:29:22 -0600

    Reverted Login component redirect changes

--- mission-control-client/src/components/auth/Login.jsx
+++ mission-control-client/src/components/auth/Login.jsx
@@ -44,7 +44,7 @@ export default withAuth(class Login extends Component {
   render() {
     if (this.state.authenticated === null) return null;
     return this.state.authenticated ?
-      <Redirect to={{ pathname: '/admin/dashboard' }}/> :
+      <Redirect to={{ pathname: '/' }}/> :
       <SignInWidget
         baseUrl={this.props.baseUrl}
         onSuccess={this.onSuccess}
